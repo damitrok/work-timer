0bc6b349f76f4f90a203e36e3a3da656e32d2868
damitrokhin <damitrokhin@lanit.ru>
2024-07-13 15:20:15 +0300

    feat(components): добавлен скелет приложения

---
+++ src/components/buttonsPanel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const buttonsPanel = () => {
+  return (
+    <div>
+      <button>Start</button>
+      <button>Stop</button>
+    </div>
+  );
+};
+
+export default buttonsPanel;
---
+++ src/components/clock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const clock = () => {
+    return (
+        <div>
+            <div> 12 : 50</div>
+        </div>
+    );
+};
+
+export default clock;
